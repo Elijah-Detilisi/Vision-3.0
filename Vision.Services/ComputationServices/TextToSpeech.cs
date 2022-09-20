f633a5b124212fbbc71aab55be3d29c20655dce7
Philani Mbatha <PhilaniM@hollywoodbets.net>
2022-09-20 16:36:09 +0200

    add audio service

---
+++ Vision.Services/ComputationServices/TextToSpeech.cs
@@ -0,0 +1,56 @@
+﻿
+#pragma warning disable CA1416 // Validate platform compatibility
+
+namespace Vision.Services.ComputationServices
+{
+    using System.Speech.Synthesis;
+
+    public class TextToSpeech
+    {
+        #region Instances
+        private readonly SpeechSynthesizer _speechSynthesizer;
+        #endregion
+
+        public TextToSpeech()
+        {
+            _speechSynthesizer = new SpeechSynthesizer();
+        }
+
+        #region Setter Methods
+        public void SetAudioSpeed(int rate)
+        {
+            _speechSynthesizer.Rate = rate;
+        }
+
+        public void SetAudioVolume(int volume)
+        {
+            _speechSynthesizer.Volume = volume;
+        }
+        #endregion
+
+        #region Pronouncation Methods
+        public void Speak(string text)
+        {
+            _speechSynthesizer.Speak(text);
+        }
+        public async Task SpeakAsync(string text)
+        {
+            _speechSynthesizer.SpeakAsync(text);
+        }
+        #endregion
+
+        #region Cancellation Methods
+        public void Dispose()
+        {
+            _speechSynthesizer.Pause();
+            _speechSynthesizer.Dispose();
+        }
+        public void StopAsync()
+        {
+            _speechSynthesizer.SpeakAsyncCancelAll();
+        }
+        #endregion
+    }
+}
+
+#pragma warning restore CA1416 // Validate platform compatibility
---
+++ Vision.Services/ComputationServices/Vocabulary.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Vision.Services.ComputationServices
+{
+    public static class Vocabulary
+    {
+        #region Data fields
+        private readonly static Dictionary<string, string> _promptMessages = new Dictionary<string, string>(){
+            {
+                "Security: Alert",
+                    "Intruder alert please step away from the computer!"
+            },
+            {
+                "Security: Launch",
+                    "Vision security system engaged!"
+            }
+        };
+
+        private readonly static Dictionary<string, string[]> _commands = new Dictionary<string, string[]>()
+        {
+            {
+                "Security: Engage",
+                    new string[]{
+                        "Engage vision", "Launch vision"
+                    }
+            }
+        };
+        #endregion
+
+        #region Class methods
+        public static string GetPromptMessage(string promptKey)
+        {
+            string message = _promptMessages[promptKey];
+
+            return message;
+        }
+
+        public static string[] GetCommands(string commandKey)
+        {
+            var commands = _commands[commandKey];
+            return commands;
+        }
+        #endregion
+
+    }
+}
